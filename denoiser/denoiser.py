65a11d6bcd8f6e22cb1662592c2e4db390c4f89e
wrathchild14 <jovanprodanov123@gmail.com>
2023-05-18 18:53:27 +0200

    rename, fix
    normalizations are wrong

--- denoiser/denoiser.py
+++ denoiser/denoiser.py
@@ -29,9 +29,9 @@ class ResNetBlock(nn.Module):
         return out
 
 
-class DenoisingDiffusionNetworkEncDec(nn.Module):
+class DenoiserEncDec(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(DenoisingDiffusionNetworkEncDec, self).__init__()
+        super(DenoiserEncDec, self).__init__()
 
         self.encoder = nn.Sequential(
             nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=1),
@@ -70,54 +70,57 @@ class DenoisingDiffusionNetworkEncDec(nn.Module):
         return decoded
 
 
-class DenoisingDiffusionNetworkOld(nn.Module):
-    def __init__(self, in_channels, out_channels, num_blocks=4):
-        super(DenoisingDiffusionNetworkOld, self).__init__()
+class DenoiserDiffusion(nn.Module):
+    def __init__(self, in_channels, out_channels, num_steps=10):
+        super(DenoiserDiffusion, self).__init__()
 
-        self.encoder = resnet18(pretrained=False)
-        self.encoder.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=1, padding=3, bias=False)
+        self.num_steps = num_steps
 
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=True)
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
 
-        self.res_blocks = nn.ModuleList([ResNetBlock(64, 64) for _ in range(num_blocks)])
-
-        self.skip_connections = nn.ModuleList([
-            nn.Conv2d(64, 64, kernel_size=1)  # Adjusted number of input channels here
-            for _ in range(num_blocks)
+        self.diffusion_steps = nn.ModuleList([
+            nn.Sequential(
+                nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(inplace=True)
+            ) for _ in range(num_steps)
         ])
 
-        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(64, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.Tanh()
+        )
 
     def forward(self, x):
-        x = self.encoder.conv1(x)
-        x = self.encoder.bn1(x)
-        x = self.encoder.relu(x)
-        x = self.encoder.maxpool(x)
+        encoded = self.encoder(x)
 
-        enc_features = []
-        x = self.encoder.layer1(x)
-        enc_features.append(x)
-        x = self.encoder.layer2(x)
-        enc_features.append(x)
-        x = self.encoder.layer3(x)
-        enc_features.append(x)
-        x = self.encoder.layer4(x)
-        enc_features.append(x)
+        for i in range(self.num_steps):
+            encoded = self.diffusion_steps[i](encoded)
 
-        x = self.decoder(x)
-
-        for i, res_block in enumerate(self.res_blocks):
-            x = res_block(x)
-            skip_connection = self.skip_connections[i](torch.cat([x, enc_features[i]], dim=1))
-            x += skip_connection
-
-        x = self.final_conv(x)
-
-        return x
+        decoded = self.decoder(encoded)
+        return decoded
--- denoiser/run_train_model.py
+++ denoiser/run_train_model.py
@@ -1,23 +1,23 @@
-import torch
 import torch.optim as optim
+import torch.nn.functional
+
+from torch.nn.functional import mse_loss
 from torchvision import transforms
 from torch.utils.data import DataLoader
-
-from denoiser import DenoisingDiffusionNetworkEncDec
+from denoiser import DenoiserEncDec, DenoiserDiffusion
 from denoiser_dataset import DenoiserDataset
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     transform = transforms.Compose([
+        transforms.ToTensor(),
+        # transforms.Resize((256, 256)),
+        # transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
         # transforms.RandomHorizontalFlip(),
         # transforms.RandomVerticalFlip(),
         # transforms.RandomRotation(30),
         # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-        transforms.ToTensor(),
-        # transforms.Resize((256, 256)),
-        # transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-        transforms.Normalize(mean=(0.5/255, 0.5/255, 0.5/255), std=(0.5/255, 0.5/255, 0.5/255))
     ])
     clean_dir = "./data/clean"
     noisy_dir = "./data/noisy"
@@ -27,25 +27,33 @@ if __name__ == "__main__":
     in_channels = 3
     out_channels = 3
 
-    model = DenoisingDiffusionNetworkEncDec(in_channels, out_channels).to(device)
+    # model = DenoiserDiffusion(in_channels, out_channels).to(device)
+    model = DenoiserEncDec(in_channels, out_channels).to(device)
     criterion = torch.nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
     # load prev network
-    model.load_state_dict(torch.load("trained_diffusion_model.pth"))
+    # model.load_state_dict(torch.load("trained_diffusion_model.pth"))
 
-    num_epochs = 100
+    num_epochs = 10
     for epoch in range(num_epochs):
         for batch, (noisy_images, clean_images) in enumerate(dataloader):
             noisy_images = noisy_images.to(device)
             clean_images = clean_images.to(device)
 
+            # denoised_images = to_tensor(denoised_images)  # Convert denoised images to tensors
+            # clean_images = to_tensor(clean_images)
+
             denoised_images = model(noisy_images)
-
             loss = criterion(denoised_images, clean_images)
-
             optimizer.zero_grad()
-            loss.backward()
+
+            mse = mse_loss(denoised_images, clean_images)
+            # ssim_loss = 1 - ssim(denoised_images, clean_images, data_range=1, size_average=True)
+            # total_loss = 0.5 * mse + 0.5 * ssim_loss
+            # total_loss.backward()
+            mse.backward()
+
             optimizer.step()
             print(f"Epoch [{epoch + 1}/{num_epochs}], Batch [{batch + 1}/{len(dataloader)}], Loss: {loss.item()}")
 
